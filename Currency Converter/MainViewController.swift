04ac101706209c57adb927b49ee9bcafc3b468c6
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-08-04 15:14:49 +0300

    Finish up with the UI design

--- Currency Converter/MainViewController.swift
+++ Currency Converter/MainViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    private let firstCurrencyButton = UIButton()
-    private let secondCurrencyButton = UIButton()
-    private let swapCurrenciesButton = UIButton()
+    private let firstCurrencyButton = UIButton(type: .system)
+    private let secondCurrencyButton = UIButton(type: .system)
+    private let swapCurrenciesButton = UIButton(type: .system)
     private let firstCurrencyTextfield = UITextField()
     private let secondCurrencyTextfield = UITextField()
 
@@ -22,10 +22,16 @@ class MainViewController: UIViewController {
 }
 
 private extension MainViewController {
-    func setupSubviews() {
-        view.backgroundColor = .green
+    enum Dimensions {
+        static let spacing: CGFloat = 10
+        static let swapButtonWidth: CGFloat = 50
+        static let swapButtonHeight: CGFloat = 40
+    }
 
-        let swapImageView = UIImageView(image: UIImage(named: ""))
+    func setupSubviews() {
+        view.backgroundColor = .white
+
+        let swapImageView = UIImageView(image: UIImage(named: "icon-exchange"))
         let swapView = UIView()
         [swapImageView, swapCurrenciesButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
@@ -38,15 +44,13 @@ private extension MainViewController {
                 $0.bottomAnchor.constraint(equalTo: swapView.bottomAnchor),
             ])
         }
-        swapImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        swapImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        swapImageView.widthAnchor.constraint(equalToConstant: Dimensions.swapButtonWidth).isActive = true
+        swapImageView.heightAnchor.constraint(equalToConstant: Dimensions.swapButtonHeight).isActive = true
 
         let headerView = UIView()
         [firstCurrencyButton, swapView, secondCurrencyButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             headerView.addSubview($0)
-
-            $0.backgroundColor = .orange
 
             NSLayoutConstraint.activate([
                 $0.topAnchor.constraint(equalTo: headerView.topAnchor),
@@ -55,33 +59,40 @@ private extension MainViewController {
         }
         NSLayoutConstraint.activate([
             firstCurrencyButton.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
-            swapView.leadingAnchor.constraint(equalTo: firstCurrencyButton.trailingAnchor, constant: 10),
-            secondCurrencyButton.leadingAnchor.constraint(equalTo: swapView.trailingAnchor, constant: 10),
+            swapView.leadingAnchor.constraint(equalTo: firstCurrencyButton.trailingAnchor, constant: Dimensions.spacing),
+            secondCurrencyButton.leadingAnchor.constraint(equalTo: swapView.trailingAnchor, constant:  Dimensions.spacing),
             secondCurrencyButton.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
 
             firstCurrencyButton.widthAnchor.constraint(equalTo: secondCurrencyButton.widthAnchor, multiplier: 1),
         ])
-        swapView.backgroundColor = .brown
 
         [firstCurrencyTextfield, secondCurrencyTextfield].forEach {
             $0.keyboardType = .decimalPad
-            $0.backgroundColor = .blue
+            $0.borderStyle = .roundedRect
         }
 
         let textfieldsStackview = UIStackView(arrangedSubviews: [firstCurrencyTextfield, secondCurrencyTextfield])
         textfieldsStackview.axis = .horizontal
-        textfieldsStackview.spacing = 30
+        textfieldsStackview.spacing = Dimensions.swapButtonWidth + (2 * Dimensions.spacing)
         textfieldsStackview.distribution = .fillEqually
 
         let stackview = UIStackView(arrangedSubviews: [headerView, textfieldsStackview])
         stackview.axis = .vertical
-        stackview.spacing = 10
+        stackview.spacing = Dimensions.spacing
+        stackview.distribution = .fillEqually
         stackview.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(stackview)
         NSLayoutConstraint.activate([
-            stackview.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            stackview.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackview.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Dimensions.spacing),
+            stackview.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Dimensions.spacing),
+            stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Dimensions.swapButtonHeight),
         ])
+
+        setDefaults()
+    }
+
+    func setDefaults() {
+        firstCurrencyButton.setTitle("US Dollar", for: .normal)
+        secondCurrencyButton.setTitle("Euro", for: .normal)
     }
 }

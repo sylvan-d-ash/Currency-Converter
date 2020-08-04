524eedcfb3ce79f2b11bacb213a2d5cf6a677f5a
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-08-04 14:02:43 +0300

    Change to declare root view controller programmatically

--- Currency Converter/MainViewController.swift
+++ Currency Converter/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Currency Converter
 //
 //  Created by Sylvan Ash on 04/08/2020.
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupSubviews()
     }
-
-
 }
 
+private extension MainViewController {
+    func setupSubviews() {
+        view.backgroundColor = .green
+    }
+}

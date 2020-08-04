b5689d6a526aedb589cfac0a8ceedf1b3eb8ad3e
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-08-04 15:00:31 +0300

    Default layout of views

--- Currency Converter/MainViewController.swift
+++ Currency Converter/MainViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    private let firstCurrencyButton = UIButton()
+    private let secondCurrencyButton = UIButton()
+    private let swapCurrenciesButton = UIButton()
+    private let firstCurrencyTextfield = UITextField()
+    private let secondCurrencyTextfield = UITextField()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSubviews()
@@ -18,5 +24,64 @@ class MainViewController: UIViewController {
 private extension MainViewController {
     func setupSubviews() {
         view.backgroundColor = .green
+
+        let swapImageView = UIImageView(image: UIImage(named: ""))
+        let swapView = UIView()
+        [swapImageView, swapCurrenciesButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            swapView.addSubview($0)
+
+            NSLayoutConstraint.activate([
+                $0.leadingAnchor.constraint(equalTo: swapView.leadingAnchor),
+                $0.trailingAnchor.constraint(equalTo: swapView.trailingAnchor),
+                $0.topAnchor.constraint(equalTo: swapView.topAnchor),
+                $0.bottomAnchor.constraint(equalTo: swapView.bottomAnchor),
+            ])
+        }
+        swapImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        swapImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+
+        let headerView = UIView()
+        [firstCurrencyButton, swapView, secondCurrencyButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            headerView.addSubview($0)
+
+            $0.backgroundColor = .orange
+
+            NSLayoutConstraint.activate([
+                $0.topAnchor.constraint(equalTo: headerView.topAnchor),
+                $0.bottomAnchor.constraint(equalTo: headerView.bottomAnchor),
+            ])
+        }
+        NSLayoutConstraint.activate([
+            firstCurrencyButton.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
+            swapView.leadingAnchor.constraint(equalTo: firstCurrencyButton.trailingAnchor, constant: 10),
+            secondCurrencyButton.leadingAnchor.constraint(equalTo: swapView.trailingAnchor, constant: 10),
+            secondCurrencyButton.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
+
+            firstCurrencyButton.widthAnchor.constraint(equalTo: secondCurrencyButton.widthAnchor, multiplier: 1),
+        ])
+        swapView.backgroundColor = .brown
+
+        [firstCurrencyTextfield, secondCurrencyTextfield].forEach {
+            $0.keyboardType = .decimalPad
+            $0.backgroundColor = .blue
+        }
+
+        let textfieldsStackview = UIStackView(arrangedSubviews: [firstCurrencyTextfield, secondCurrencyTextfield])
+        textfieldsStackview.axis = .horizontal
+        textfieldsStackview.spacing = 30
+        textfieldsStackview.distribution = .fillEqually
+
+        let stackview = UIStackView(arrangedSubviews: [headerView, textfieldsStackview])
+        stackview.axis = .vertical
+        stackview.spacing = 10
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackview)
+        NSLayoutConstraint.activate([
+            stackview.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackview.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        ])
     }
 }

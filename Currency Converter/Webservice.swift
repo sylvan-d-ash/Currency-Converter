cb1befd7d3312850507658f6a86002503dcc1b7b
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-08-05 15:33:03 +0300

    Add generic request function

--- Currency Converter/Webservice.swift
+++ Currency Converter/Webservice.swift
@@ -28,6 +28,33 @@ enum NetworkError: Error {
 class Webservice {
     private static let API_KEY = "b29c42cb194f63237334bcecb5d86f12"
 
+    func makeRequest(endpoint: Endpoint, completion: @escaping(Result<Any, Error>) -> Void) {
+        guard let url = buildQueryURL(endpoint: endpoint) else {
+            completion(.failure(NetworkError.invalidRequest))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { dataOrNil, responseOrNil, errorOrNil in
+            if let error = errorOrNil {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = dataOrNil else {
+                completion(.failure(NetworkError.invalidResponse))
+                return
+            }
+
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print(json)
+                completion(.success(json))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+
     func fetchAllCurrencies(completion: @escaping(Result<Any, Error>) -> Void) {
         guard let url = buildQueryURL(endpoint: .currencies) else {
             completion(.failure(NetworkError.invalidRequest))

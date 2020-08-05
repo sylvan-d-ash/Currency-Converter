01de246509cac98adc2a909655fa2aaecb744eb3
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-08-05 12:28:42 +0300

    Change design

--- Currency Converter/MainViewController.swift
+++ Currency Converter/MainViewController.swift
@@ -15,6 +15,10 @@ class MainViewController: UIViewController {
     private let firstCurrencyTextfield = UITextField()
     private let secondCurrencyTextfield = UITextField()
 
+    private let currencyButton = UIButton(type: .system)
+    private let currencyTextfield = UITextField()
+    private let tableView = UITableView(frame: .zero, style: .plain)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSubviews()
@@ -31,68 +35,80 @@ private extension MainViewController {
     func setupSubviews() {
         view.backgroundColor = .white
 
-        let swapImageView = UIImageView(image: UIImage(named: "icon-exchange"))
-        let swapView = UIView()
-        [swapImageView, swapCurrenciesButton].forEach {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            swapView.addSubview($0)
+        let container = UIView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(container)
+        NSLayoutConstraint.activate([
+            container.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Dimensions.spacing),
+            container.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Dimensions.spacing),
+            container.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Dimensions.spacing),
+            container.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
 
-            NSLayoutConstraint.activate([
-                $0.leadingAnchor.constraint(equalTo: swapView.leadingAnchor),
-                $0.trailingAnchor.constraint(equalTo: swapView.trailingAnchor),
-                $0.topAnchor.constraint(equalTo: swapView.topAnchor),
-                $0.bottomAnchor.constraint(equalTo: swapView.bottomAnchor),
-            ])
-        }
-        swapImageView.widthAnchor.constraint(equalToConstant: Dimensions.swapButtonWidth).isActive = true
-        swapImageView.heightAnchor.constraint(equalToConstant: Dimensions.swapButtonHeight).isActive = true
+        tableView.dataSource = self
+        container.addSubview(tableView)
 
         let headerView = UIView()
-        [firstCurrencyButton, swapView, secondCurrencyButton].forEach {
+        container.addSubview(headerView)
+
+        currencyTextfield.delegate = self
+        currencyTextfield.keyboardType = .decimalPad
+        currencyTextfield.borderStyle = .roundedRect
+        headerView.addSubview(currencyTextfield)
+
+        currencyButton.addTarget(self, action: #selector(chooseCurrencyTapped), for: .touchUpInside)
+        currencyButton.setTitle("USD", for: .normal)
+        headerView.addSubview(currencyButton)
+
+        [currencyTextfield, currencyButton, tableView, headerView].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            headerView.addSubview($0)
-
-            NSLayoutConstraint.activate([
-                $0.topAnchor.constraint(equalTo: headerView.topAnchor),
-                $0.bottomAnchor.constraint(equalTo: headerView.bottomAnchor),
-            ])
         }
         NSLayoutConstraint.activate([
-            firstCurrencyButton.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
-            swapView.leadingAnchor.constraint(equalTo: firstCurrencyButton.trailingAnchor, constant: Dimensions.spacing),
-            secondCurrencyButton.leadingAnchor.constraint(equalTo: swapView.trailingAnchor, constant:  Dimensions.spacing),
-            secondCurrencyButton.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
+            currencyTextfield.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
+            currencyTextfield.topAnchor.constraint(equalTo: headerView.topAnchor),
+            currencyTextfield.bottomAnchor.constraint(equalTo: headerView.bottomAnchor),
 
-            firstCurrencyButton.widthAnchor.constraint(equalTo: secondCurrencyButton.widthAnchor, multiplier: 1),
+            currencyButton.leadingAnchor.constraint(equalTo: currencyTextfield.trailingAnchor, constant: Dimensions.spacing),
+            currencyButton.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
+            currencyButton.topAnchor.constraint(equalTo: headerView.topAnchor),
+            currencyButton.bottomAnchor.constraint(equalTo: headerView.bottomAnchor),
+            currencyButton.widthAnchor.constraint(equalTo: headerView.widthAnchor, multiplier: 1/6),
+
+            headerView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            headerView.topAnchor.constraint(equalTo: container.topAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 40),
+
+            tableView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: Dimensions.spacing),
+            tableView.bottomAnchor.constraint(equalTo: container.bottomAnchor),
         ])
-
-        [firstCurrencyTextfield, secondCurrencyTextfield].forEach {
-            $0.keyboardType = .decimalPad
-            $0.borderStyle = .roundedRect
-        }
-
-        let textfieldsStackview = UIStackView(arrangedSubviews: [firstCurrencyTextfield, secondCurrencyTextfield])
-        textfieldsStackview.axis = .horizontal
-        textfieldsStackview.spacing = Dimensions.swapButtonWidth + (2 * Dimensions.spacing)
-        textfieldsStackview.distribution = .fillEqually
-
-        let stackview = UIStackView(arrangedSubviews: [headerView, textfieldsStackview])
-        stackview.axis = .vertical
-        stackview.spacing = Dimensions.spacing
-        stackview.distribution = .fillEqually
-        stackview.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stackview)
-        NSLayoutConstraint.activate([
-            stackview.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Dimensions.spacing),
-            stackview.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Dimensions.spacing),
-            stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Dimensions.swapButtonHeight),
-        ])
-
-        setDefaults()
     }
 
     func setDefaults() {
         firstCurrencyButton.setTitle("US Dollar", for: .normal)
         secondCurrencyButton.setTitle("Euro", for: .normal)
+    }
+
+    @objc func chooseCurrencyTapped() {
+        //
+    }
+}
+
+extension MainViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(false)
+    }
+}
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }

7193f73d46fd73647e30ba65835fd77ffe943020
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-08-06 14:48:40 +0300

    Add test files

---
+++ Currency ConverterTests/MainPresenterTests.swift
@@ -0,0 +1,14 @@
+//
+//  MainPresenterTests.swift
+//  Currency ConverterTests
+//
+//  Created by Sylvan Ash on 06/08/2020.
+//  Copyright © 2020 Sylvan Ash. All rights reserved.
+//
+
+import XCTest
+@testable import Currency_Converter
+
+class MainPresenterTests: XCTestCase {
+    //
+}

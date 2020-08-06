185d49a56bd3c814cb79c1856aa23b91e71c23d1
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-08-06 15:21:08 +0300

    Outline tests to be done for MainPresenter

--- Currency ConverterTests/MainPresenterTests.swift
+++ Currency ConverterTests/MainPresenterTests.swift
@@ -10,5 +10,47 @@ import XCTest
 @testable import Currency_Converter
 
 class MainPresenterTests: XCTestCase {
-    //
+    var sut: MainPresenter!
+    var view: MainViewSpy!
+    var interactor: MainInteractorMock!
+
+    override func setUpWithError() throws {
+        view = MainViewSpy()
+        interactor = MainInteractorMock()
+        sut = MainPresenter(view: view, interactor: interactor)
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        interactor = nil
+        view = nil
+    }
+
+    func testViewDidLoadSuccess() throws {
+        //
+    }
+
+    func testViewDidLoadFailure() throws {
+        //
+    }
+
+    func testPickerViewWithNoCurrencies() throws {
+        //
+    }
+
+    func testPickerViewWithCurrencies() throws {
+        //
+    }
+
+    func testCurrencySelected() throws {
+        //
+    }
+
+    func testValueChanged() throws {
+        //
+    }
+
+    func testConfiguringCell() throws {
+        //
+    }
 }
